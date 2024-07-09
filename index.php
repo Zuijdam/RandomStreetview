824af4f746e6d47d7066155fb1b1e4a2fce865d5
Erik v Z <erik@netninja.nl>
2024-07-09 12:56:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Random URL</title>
+</head>
+<body>
+    <?php
+    // Read the JSON data from the file
+    $jsonData = file_get_contents('data/locations.json');
 
-// JSON uit de lijst
-$jsonData = file_get_contents('data/locations.json');
+    // Decode the JSON data into an associative array
+    $data = json_decode($jsonData, true);
 
-// JSON naar een arrau
-$data = json_decode($jsonData, true);
+    // Get a random URL from the list
+    $randomIndex = array_rand($data['locations']);
+    $randomUrl = $data['locations'][$randomIndex]['url'];
+    ?>
 
-// Random er 1tje kiezen
-$randomIndex = array_rand($data['locations']);
-$randomUrl = $data['locations'][$randomIndex]['url'];
-
-// Redirecten maar
-header('Location: ' . $randomUrl);
-exit;
-
-?>
+    <iframe src="<?php echo $randomUrl; ?>" width="100%" height="1000px" frameborder="0"></iframe>
+</body>
+</html>
