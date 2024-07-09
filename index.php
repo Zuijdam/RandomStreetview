a092f5c15381235263eebd066c2841895626575f
Erik v Z <erik@netninja.nl>
2024-07-09 12:48:10 +0200

    versie 0.1

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+// JSON uit de lijst
+$jsonData = file_get_contents('data/locations.json');
+
+// JSON naar een arrau
+$data = json_decode($jsonData, true);
+
+// Random er 1tje kiezen
+$randomIndex = array_rand($data['locations']);
+$randomUrl = $data['locations'][$randomIndex]['url'];
+
+// Redirecten maar
+header('Location: ' . $randomUrl);
+exit;
+
+?>
