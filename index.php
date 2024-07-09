41887001d33ed7397d0bd3526ac7c5fd082e387f
Erik v Z <erik@netninja.nl>
2024-07-09 20:45:57 +0200

    lijst en knop

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php
-
 // JSON uit de lijst
 $jsonData = file_get_contents('data/locations.json');
 
-// JSON naar een arrau
+// JSON naar een array
 $data = json_decode($jsonData, true);
 
-// Random er 1tje kiezen
-$randomIndex = array_rand($data['locations']);
-$randomUrl = $data['locations'][$randomIndex]['url'];
+// Check if data is not empty
+if (!empty($data['locations'])) {
+    // Display the list of locations
+    echo '<ul>';
+    foreach ($data['locations'] as $location) {
+        echo '<li><a href="' . $location['url'] . '" target="_blank">' . $location['name'] . '</a></li>';
+    }
+    echo '</ul>';
 
-// Redirecten maar
-header('Location: ' . $randomUrl);
-exit;
-
+    // Button to redirect to a random location
+    $randomIndex = array_rand($data['locations']);
+    $randomUrl = $data['locations'][$randomIndex]['url'];
+    echo '<button onclick="window.open(\'' . $randomUrl . '\', \'_blank\')">Show me a random location</button>';
+} else {
+    echo 'No locations found.';
+}
 ?>
