782101bf0afbb5c5012eab6055e08609805738eb
Erik van Zuijdam <35580176+Zuijdam@users.noreply.github.com>
2024-07-21 22:40:26 +0200

    Create api.php

---
+++ api.php
@@ -0,0 +1,13 @@
+<?php
+
+// Read the JSON file
+$jsonData = file_get_contents('/data/locations.json');
+$data = json_decode($jsonData, true);
+
+// Get a random location
+$randomLocation = $data['locations'][array_rand($data['locations'])];
+
+// Return the location as JSON
+header('Content-Type: application/json');
+echo json_encode($randomLocation);
+?>
